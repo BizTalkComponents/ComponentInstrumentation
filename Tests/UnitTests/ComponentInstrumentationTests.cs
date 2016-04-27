68fedd0b662301424e0458c753b407f6c985a31e
Robin Hultman <robin.hultman@gmail.com>
2016-04-27 07:32:46 +0200

    Added unit tests

--- Tests/UnitTests/ComponentInstrumentationTests.cs
+++ Tests/UnitTests/ComponentInstrumentationTests.cs
@@ -9,11 +9,42 @@ namespace BizTalkComponents.Utilities.Tests.UnitTests
     public class ComponentInstrumentationTests
     {
         [TestMethod]
-        public void TestTrackException()
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestNullTracker()
+        {
+            var helper = new ComponentInstrumentationHelper(null, "componentName");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestEmptyName()
         {
             var mock = new Mock<IComponentTracker>();
-            mock.Setup(componentTracker => componentTracker.TrackComponentException(new ArgumentException("exception"), DateTime.Now, new TimeSpan(1000),"componentName", "componentVersion"));
-           
+            var helper = new ComponentInstrumentationHelper(mock.Object, string.Empty);
+        }
+
+        [TestMethod]
+        public void TestError()
+        {
+            var mock = new Mock<IComponentTracker>();
+            mock.Setup(componentTracker =>
+                componentTracker.TrackComponentError(It.IsAny<DateTime>(), It.IsAny<TimeSpan>(),
+                It.IsAny<Exception>(), It.IsAny<string>(), It.IsAny<string>())).Verifiable();
+
+            var helper = new ComponentInstrumentationHelper(mock.Object, "componentName");
+            helper.TrackComponentError(new ArgumentException());
+        }
+
+        [TestMethod]
+        public void TestSuccess()
+        {
+            var mock = new Mock<IComponentTracker>();
+            mock.Setup(componentTracker =>
+                componentTracker.TrackComponentSuccess(It.IsAny<DateTime>(), It.IsAny<TimeSpan>(),
+                It.IsAny<string>(), It.IsAny<string>())).Verifiable();
+
+            var helper = new ComponentInstrumentationHelper(mock.Object, "componentName");
+            helper.TrackComponentSuccess();
         }
     }
 }
